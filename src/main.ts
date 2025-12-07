59ea1ce1c2d0641ec77ab7b5ff5219c5b4dd3ab5
mac <winfred.adrah@iisysgroup.com>
2025-12-07 08:25:56 +0000

    Added Dynamic URL

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   }));
   
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');  // 👈 ADD '0.0.0.0' HERE
   console.log(`Application is running on port ${port}`);
 }
 bootstrap();
